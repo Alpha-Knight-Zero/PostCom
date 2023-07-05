d135552f9432df32fa2b5cb654cf1aad538565f1
Alpha-Knight_Zero <pushkaldkpandey333@gmail.com>
2023-07-06 04:05:38 +0530

    included dotenc config

--- config/db.js
+++ config/db.js
@@ -1,19 +1,20 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
+require('dotenv').config();
 
 const connectDB = async () => {
-  try {
-    const conn = await mongoose.connect(
-      "mongodb+srv://adityaagr694:adityaagr694@database.iy6osys.mongodb.net/learnFlow?retryWrites=true&w=majority",
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      }
-    );
-    console.log(`MongoDB connected ${conn.connection.host}`);
-  } catch (error) {
-    console.log(error);
-    process.exit(1);
-  }
+	try {
+		const conn = await mongoose.connect(
+			'mongodb+srv://adityaagr694:adityaagr694@database.iy6osys.mongodb.net/learnFlow?retryWrites=true&w=majority',
+			{
+				useNewUrlParser: true,
+				useUnifiedTopology: true,
+			}
+		);
+		console.log(`MongoDB connected ${conn.connection.host}`);
+	} catch (error) {
+		console.log(error);
+		process.exit(1);
+	}
 };
 
 module.exports = connectDB;
