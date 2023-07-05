7ddf2bc83c5cb8d1bc80b8172983073659675152
Alpha-Knight_Zero <pushkaldkpandey333@gmail.com>
2023-07-06 04:41:51 +0530

    added PORT variable for Render deployment

--- app.js
+++ app.js
@@ -46,6 +46,8 @@ app.get('/*', (req, res) => {
 	res.render('error-404');
 });
 
-app.listen(3000, () => {
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
 	console.log('Server is running on Port 3000');
 });
