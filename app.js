648f76be8fe013d09ee137a1acc88aa6d8d8fb0d
Alpha-Knight_Zero <pushkaldkpandey333@gmail.com>
2023-08-29 07:44:28 +0530

    made PORT a variable on server side

--- app.js
+++ app.js
@@ -49,5 +49,5 @@ app.get('/*', (req, res) => {
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-	console.log('Server is running on Port 3000');
+	console.log(`Server is running on Port ${PORT}`);
 });
